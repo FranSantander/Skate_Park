5ed39e4ee1474c8318756079eb1ab7b4e546d525
FranSantander <naturatiendacopiapo@gmail.com>
2022-05-04 10:58:18 -0400

    vista datos

--- consultas.js
+++ consultas.js
@@ -7,17 +7,89 @@ const pool = new Pool({
   port: 5432,
 });
 
-async function nuevoSkater(email, nombre, password, experiencia, especialidad, foto) {
+async function consultarSkaters() {
   try {
-      const result = await pool.query(
-          `INSERT INTO skaters
-          (email, nombre, password, anos_experiencia, especialidad, foto, estado)
-          VALUES ('${email}','${nombre}','${password}','${experiencia}','${especialidad}','${foto}',false) RETURNING *`);
-      console.log(`Usuario ${result.rows[0].nombre} creado con éxito`);
-      return result.rows;
-  } catch (e) {
-      console.log(e);
+    const result = await pool.query(`SELECT * FROM skaters`);
+    return result.rows;
+  } catch (error) {
+    console.log(error);
   }
 }
 
-module.exports = { nuevoSkater };
+async function nuevoSkater(
+  email,
+  nombre,
+  password,
+  experiencia,
+  especialidad,
+  foto
+) {
+  try {
+    const result = await pool.query(
+      `INSERT INTO skaters
+          (email, nombre, password, anos_experiencia, especialidad, foto, estado)
+          VALUES ('${email}','${nombre}','${password}','${experiencia}','${especialidad}','${foto}',false) RETURNING *`
+    );
+    //console.log(`Usuario ${result.rows[0].nombre} creado con éxito`);
+    return result.rows[0];
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function getSkaters(email, password) {
+  const result = await pool.query(
+    `SELECT * FROM skaters WHERE email = '${email}' AND password = '${password}'`
+  );
+  return result.rows;
+}
+async function updateStatus(id, estado) {
+  const result = await pool.query(
+    `UPDATE skaters SET estado = ${estado} WHERE id = ${id} RETURNING *;`
+  );
+  //console.log(`Estado del usuario '${result.rows[0].nombre}' actualizado con éxito`);
+  const usuario = result.rows[0];
+  return usuario;
+}
+async function updateSkater(
+  email,
+  nombre,
+  password,
+  experiencia,
+  especialidad
+) {
+  try {
+    const result = await pool.query(
+      `UPDATE skaters SET nombre = '${nombre}', password = '${password}', anos_experiencia = '${experiencia}', especialidad = '${especialidad}' WHERE email = '${email}' RETURNING *`
+    );
+    console.log(`Usuario ${result.rows[0].nombre} actualizado con éxito`);
+    return result.rows;
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function getFoto(email) {
+  try {
+      const result = await pool.query(`SELECT foto FROM skaters WHERE email = '${email}'`)
+      return result.rows[0];
+  } catch (error) {
+      console.log(error);
+  }
+}
+async function deleteUsuario(email) {
+  try {
+      const result = await pool.query(`DELETE FROM skaters WHERE email = '${email}' RETURNING *`);
+      console.log('Usuario eliminado con éxito');
+      return result.rows;
+  } catch (error) {
+      console.log(error);
+  }
+}
+module.exports = {
+  consultarSkaters,
+  nuevoSkater,
+  getSkaters,
+  updateStatus,
+  updateSkater,
+  getFoto,
+  deleteUsuario,
+};
--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 //const path = require("path");
 const secretKey = "secreto";
-const { nuevoSkater } = require("./consultas");
+const {
+  consultarSkaters,
+  nuevoSkater,
+  getSkaters,
+  updateStatus,
+  updateSkater,
+  getFoto,
+  deleteUsuario,
+} = require("./consultas");
 const app = express();
 
 app.listen(3000, console.log("Server on port 3000"));
@@ -30,23 +38,31 @@ app.engine(
   exphbs.engine({
     defaultLayout: "main",
     layoutsDir: `${__dirname}/views/layout`,
+    helpers: {
+      fixInde: function (valor) {
+        return parseInt(valor) + 1;
+      },
+    },
   })
 );
 app.set("view engine", "handlebars");
 
-//Ruta Home
-app.get("/", (req, res) => {
-  res.render("index");
+//Ruta raíz
+app.get("/", async (req, res) => {
+  try {
+    const usuarios = await consultarSkaters();
+    res.render("Index", { usuarios });
+  } catch (error) {
+    res.status(500).send({
+      error: `Algo salió mal... ${error}`,
+      code: 500,
+    });
+  }
 });
 
 //Ruta para ver Registro
 app.get("/registro", function (req, res) {
   res.render("Registro");
-});
-
-//Ruta para ver Login
-app.get("/login", function (req, res) {
-  res.render("Login");
 });
 
 //Ruta para ingresar nuevo usuario
@@ -67,6 +83,132 @@ app.post("/registrar", async (req, res) => {
         if (err) res.status(500).send("Error al subir la imagen");
         res.status(201).redirect("/login");
       });
+    });
+  } catch (error) {
+    res.status(500).send({
+      error: `Algo salió mal... ${error}`,
+      code: 500,
+    });
+  }
+});
+
+//Ruta para ver Login
+app.get("/login", function (req, res) {
+  res.render("Login");
+});
+
+//Ruta para iniciar sesión
+app.post("/verify", async function (req, res) {
+  const { email, password } = req.body;
+  const user = await getSkaters(email, password);
+  if (user) {
+    if (user.estado) {
+      const token = jwt.sign(
+        {
+          exp: Math.floor(Date.now() / 1000) + 180,
+          data: user,
+        },
+        secretKey
+      );
+      res.send(token);
+    } else {
+      res.status(401).send({
+        error: "Este usuario aún no ha sido validado para subir imágenes",
+        code: 401,
+      });
+    }
+  } else {
+    res.status(404).send({
+      error: "Este usuario no está registrado en la base de datos",
+      code: 404,
+    });
+  }
+});
+//Ruta para modificar el estado los usuarios
+app.put("/usuarios", async (req, res) => {
+  const { id, estado } = req.body;
+  try {
+    const usuario = await updateStatus(id, estado);
+    res.status(200).send(usuario);
+  } catch (error) {
+    res.status(500).send({
+      error: `Algo salió mal... ${error}`,
+      code: 500,
+    });
+  }
+});
+
+//Ruta de los administradores
+app.get("/Admin", async (req, res) => {
+  try {
+    const usuarios = await consultarSkaters();
+    res.render("Admin", { usuarios });
+  } catch (error) {
+    res.status(500).send({
+      error: `Algo salió mal... ${error}`,
+      code: 500,
+    });
+  }
+});
+
+//Ruta modificar datos usuarios
+app.get("/Datos", (req, res) => {
+  const { token } = req.query;
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (decoded === undefined) {
+      return res.status(401).send({
+        error: "401 Unauthorized",
+        message: "Usted no esta autorizado para estar aqui",
+      });
+    }
+    const { data } = decoded;
+    const { email, nombre, password, anos_experiencia, especialidad } =
+      data;
+    err
+      ? res.status(401).send({
+          error: "401 Unauthorized",
+          message: "Usted no esta autorizado para estar aqui",
+          token_error: err.message,
+        })
+      : res.render("Datos", {
+          email,
+          nombre,
+          password,
+          anos_experiencia,
+          especialidad,
+        });
+  });
+});
+
+//Ruta para actualizar un usuario
+app.put("/update", async (req, res) => {
+  const { email, nombre, password, experiencia, especialidad } = req.body;
+  try {
+    const usuario = await updateSkater(
+      email,
+      nombre,
+      password,
+      experiencia,
+      especialidad
+    );
+    res.status(200).send(usuario);
+  } catch (e) {
+    res.status(500).send({
+      error: `Algo salió mal... ${e}`,
+      code: 500,
+    });
+  }
+});
+
+//Ruta para eliminar un usuario
+app.delete("/delete/:email", async (req, res) => {
+  const { email } = req.params;
+  try {
+    const { foto } = await getFoto(email);
+    const usuario = await deleteUsuario(email);
+    fs.unlink(`${__dirname}/public/uploads/${foto}`, (err) => {
+      if (err) console.log(err);
+      res.status(200).send(usuario);
     });
   } catch (e) {
     res.status(500).send({
